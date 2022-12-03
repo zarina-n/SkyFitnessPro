104905db06210967536d6ac9aa22d347a1602c8b
TTurkevich <tatu_it@internet.ru>
2022-12-03 17:57:54 +0300

    add CourseCart

---
+++ src/data/CART_IMG.js
@@ -0,0 +1,29 @@
+const CART_IMG = [
+  {
+    id: 'yoga',
+    src: 'image/carts/yoga.png',
+    title: 'Йога',
+  },
+  {
+    id: 'srtetch',
+    src: 'image/carts/stretch.png',
+    title: 'Стретчинг',
+  },
+  {
+    id: 'dance',
+    src: 'image/carts/dance.png',
+    title: 'Танцевальный фитнесс',
+  },
+  {
+    id: 'step',
+    src: 'image/carts/step.png',
+    title: 'Степ',
+  },
+  {
+    id: 'bodyflex',
+    src: 'image/carts/bodyflex.png',
+    title: 'Бодифлекс',
+  },
+]
+
+export default CART_IMG
