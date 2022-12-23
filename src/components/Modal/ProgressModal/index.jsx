8af9bf5b78b5f063a63b73411a7355dc3dced17a
Yarengina <yarenginalyubava@gmail.com>
2022-12-23 21:42:33 +0300

    added exercises to ProgressModal

--- src/components/Modal/ProgressModal/index.jsx
+++ src/components/Modal/ProgressModal/index.jsx
@@ -1,10 +1,11 @@
+/* eslint-disable react/jsx-key */
 import { useForm } from 'react-hook-form'
 
 import ButtonMain from '../../Ui/ButtonMain'
 import InputProgress from '../Inputs/Progress'
 import classes from './index.module.css'
 
-const ProgressModal = ({ onClick }) => {
+const ProgressModal = ({ exercises, onClick }) => {
   const {
     register,
     handleSubmit,
@@ -14,22 +15,25 @@ const ProgressModal = ({ onClick }) => {
   const onSubmit = (data) => {
     console.log(data)
     //в input передавать имя инпута или index при map, чтобы получить значение в data;
-    //сейчас такая дата {"первый": '1', "второй": '1', "третий": '1'} единички результат заполнения формы
     onClick()
   }
+
   return (
     <form className={classes.form} onSubmit={handleSubmit(onSubmit)}>
       <h2 className={classes.title}>Мой прогресс</h2>
       <div className={classes.inputs}>
-        <label className={classes.text} />
-        Сколько раз вы сделали наклоны вперед?
-        <InputProgress name="первый" register={register} errors={errors} />
-        <label className={classes.text} />
-        Сколько раз вы сделали наклоны назад?
-        <InputProgress name="второй" register={register} errors={errors} />
-        <label className={classes.text} />
-        Сколько раз вы сделали поднятие ног, согнутых в коленях?
-        <InputProgress name="третий" register={register} errors={errors} />
+        {exercises?.map((exercise) => (
+          <label className={classes.text}>
+            {`Сколько раз вы сделали упражнение "${
+              exercise.name.split('(')[0]
+            }" ?`}
+            <InputProgress
+              name={exercise.id}
+              register={register}
+              errors={errors}
+            />
+          </label>
+        ))}
       </div>
       <ButtonMain type="submit" content="Отправить" />
     </form>
